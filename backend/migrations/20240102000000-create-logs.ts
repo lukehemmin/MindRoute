1cfee962004623b0e303b06cfd19eb352f560a65
박상호 Sangho Park <ps040211@gmail.com>
2025-04-02 10:13:26 +0900

    마이그레이션 파일 수정: 'create-logs.ts' 파일에서 CommonJS 모듈을 ES6 모듈로 변경하여 코드 일관성을 높임.

--- backend/migrations/20240102000000-create-logs.ts
+++ backend/migrations/20240102000000-create-logs.ts
@@ -1,6 +1,6 @@
 import { QueryInterface, DataTypes } from 'sequelize';
 
-module.exports = {
+export default {
   up: async (queryInterface: QueryInterface) => {
     await queryInterface.createTable('logs', {
       id: {
