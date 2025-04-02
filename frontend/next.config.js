ab6f4551f1593d59489eb379326f2ac7b70f60a8
박상호 Sangho Park <ps040211@gmail.com>
2025-04-02 19:34:12 +0900

    Next.js 설정 업데이트: API 리다이렉션을 위한 비동기 리라이트 기능 추가 및 'X-Powered-By' 헤더 비활성화로 보안성 강화.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  poweredByHeader: false,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001/api/:path*',
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig; 
